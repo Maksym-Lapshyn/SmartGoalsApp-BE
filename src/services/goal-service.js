bf71bdfc96259a62b1d9c61505c5f71681cb97a0
Maksym Lapshyn <maksym@hano.no>
2018-09-25 09:31:49 +0200

    Fixed goal repository.

--- src/services/goal-service.js
+++ src/services/goal-service.js
@@ -1,27 +1,27 @@
-import { goalModel } from '../repositories/goal-repository';
+import { goalRepository } from '../repositories/goal-repository';
 
 const create = function (goal) {
-	return goalModel.create(goal);
+	return goalRepository.create(goal);
 };
 
 const getAll = function () {
-	return goalModel.getAll();
+	return goalRepository.getAll();
 };
 
 const getSingle = function (goalId) {
-	return goalModel.getSingle(goalId);
+	return goalRepository.getSingle(goalId);
 };
 
 const update = function(goalId, goal) {
-	return goalModel.update(goalId, goal);
+	return goalRepository.update(goalId, goal);
 };
 
 const remove = function(goalId) {
-	return goalModel.remove(goalId);
+	return goalRepository.remove(goalId);
 };
 
 const checkIfExists = function(goalId) {
-	return goalModel.checkIfExists(goalId);
+	return goalRepository.checkIfExists(goalId);
 };
 
 const goalService = {
